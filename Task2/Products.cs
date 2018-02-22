a586b8f83f89f0bb67946ded864f11e1c20dcda2
Jopno <35934376+Jopno@users.noreply.github.com>
2018-02-22 13:51:24 +0200

    Add files via upload

---
+++ Task2/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    class Products
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Products> shopping = new List<Products>()
+            {
+                new Products{Name = "Potato", Price = 0.6f},
+                new Products{Name = "Minced meat", Price = 2.5f},
+                new Products{Name = "Rainbow trout", Price = 11.5f}
+            };
+
+            foreach(Products x in shopping)
+            {
+                Console.WriteLine("Product: {0} {1} euros", x.Name, x.Price);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
